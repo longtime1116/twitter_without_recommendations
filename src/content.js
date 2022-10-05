13afcb7c7d232fd8549e506b879feb7ff57b9909
shumo.tokinaga <longtime1116@gmail.com>
2022-10-05 12:29:51 +0900

    ページ遷移に対応

--- src/content.js
+++ src/content.js
@@ -1,16 +1,24 @@
+// ページロード時
 window.addEventListener("load", main, false);
 
-function main(e) {
+// その後の遷移にも対応
+var observer = new MutationObserver(main);
+observer.observe(document.getElementsByTagName("body")[0], {
+  attributes: true,
+  childList:  true
+});
+
+function main() {
+  console.log("removing twitter recommendations...")
   const checkTimer = setInterval(jsLoaded, 1000);
   let count = 0;
   function jsLoaded() {
     // 「#話題を検索」「いまどうしてる？」「おすすめユーザー」を削除
-    const labels = ["調べたいものを検索", "タイムライン: トレンド", "おすすめユーザー"]
+    const labels = ["調べたいものを検索", "タイムライン: トレンド", "おすすめユーザー"];
     let elements = [];
     labels.forEach(function(l) {
       elements.push(document.querySelector(`[aria-label="${l}"]`))
     })
-    // null とかを排除
     elements = elements.filter(e=>e)
     if (elements.length != 0) {
       elements.map(e => e.remove());
@@ -18,7 +26,9 @@ function main(e) {
       // 全部消せた
       if (count == labels.length) {
         clearInterval(checkTimer);
+        console.log("twitter recommendations are removed!")
       }
     }
   }
 };
+
