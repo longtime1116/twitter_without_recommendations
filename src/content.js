8743b0377c82123e9ef755d17d673f04424ddc18
shumo.tokinaga <longtime1116@gmail.com>
2022-10-05 15:21:27 +0900

    prettier

--- src/content.js
+++ src/content.js
@@ -1,20 +1,24 @@
 window.addEventListener("load", exec, false);
 
-var observer = new MutationObserver(exec);
+let observer = new MutationObserver(exec);
 observer.observe(document.getElementsByTagName("body")[0], {
   attributes: true,
-  subtree:  true
+  subtree: true,
 });
-console.log("----- twitter_without_recommendations loaded -----")
+console.log("----- twitter_without_recommendations loaded -----");
 
 function exec() {
   // 「#話題を検索」「いまどうしてる？」「おすすめユーザー」を削除
-  const labels = ["調べたいものを検索", "タイムライン: トレンド", "おすすめユーザー"];
-  labels.forEach(function(l) {
-    let e = document.querySelector(`[aria-label="${l}"]`)
+  const labels = [
+    "調べたいものを検索",
+    "タイムライン: トレンド",
+    "おすすめユーザー",
+  ];
+  labels.forEach(function (l) {
+    let e = document.querySelector(`[aria-label="${l}"]`);
     if (e != null) {
-      e.remove()
-      console.log(`-----  [${l}] removed!!! -----`)
+      e.remove();
+      console.log(`-----  [${l}] removed!!! -----`);
     }
-  })
-};
+  });
+}
